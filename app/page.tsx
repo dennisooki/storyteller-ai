48cd1c0531e0845531472036d7ca8a24d36ec307
dennisooki <dennisooki@gmail.com>
2024-06-23 13:03:37 +0000

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import logo from "@/images/logo.png"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="">
-      <h1>AI Agent GPTScript build</h1>
+      <section>
+        <div>
+          <Image src={logo} height={250} alt="Logo"/>
+          <Button asChild className="px-20 bg-purple-700 p-10 text-xl">
+            <Link href='/stories'>Explore Story Library</Link>
+          </Button>
+        </div>
+      </section>
     </main>
   );
 }
