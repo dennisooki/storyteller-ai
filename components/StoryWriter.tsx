f2c6b92677cd76444d0bd272418df70aac0a402b
dennisooki <dennisooki@gmail.com>
2024-06-24 16:44:21 +0000

    state management for page and stoey

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -1,31 +1,46 @@
 "use client";
 
 
+import { useState } from "react";
+import { Button } from "./ui/button";
 import { Select, SelectItem, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 import { Textarea } from "./ui/textarea";
 
 
 function StoryWriter() {
+    const [story, setStory] = useState<string>("");
+    const [pages, setPages] = useState<number>();
+
     return (
         <div className="flex flex-col container">
             <section className="flex-1 flex flex-col border
             border-purple-300 rounded-md p-10 space-y-2">
-                <Textarea
+                <Textarea 
+                value={story}
+                onChange={(e) => setStory(e.target.value)}
+                className="flex-1 text-black"
+                 placeholder="Write a story about a robot and a human who become friends"
                 />
-                <Select>
+                <Select
+                onValueChange={value => setPages(parseInt(value))}
+                >
                     <SelectTrigger>
                         <SelectValue placeholder="How many pages should the story be?"
                         />
                     </SelectTrigger>
-                    <SelectContent>
+                    <SelectContent className="w-full">
                         {Array.from({ length: 10 }, (_, i) => (
                             <SelectItem key={i} value={String(i + 1)}>
                                 {i + 1}
                             </SelectItem>
                         ))}
                     </SelectContent>
-
                 </Select>
+                <Button 
+                disabled={!story || !pages}
+                className="w-full" size='lg'>
+                    Generate Story
+                </Button>
 
             </section>
             <section className="flex-1 pb-5 mt-5">
