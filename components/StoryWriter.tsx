f434a1c75370a840b3a6636fa6e0c03fe474f094
dennisooki <dennisooki@gmail.com>
2024-06-25 07:48:09 +0000

    finshed the state manager

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -10,19 +10,22 @@ import { Textarea } from "./ui/textarea";
 function StoryWriter() {
     const [story, setStory] = useState<string>("");
     const [pages, setPages] = useState<number>();
+    const [progress, setProgress] = useState("");
+    const [runStarted, setRunStarted] = useState<boolean>(false);
+    const [runFinished, setRunFinished] = useState<boolean | null>(null);
 
     return (
         <div className="flex flex-col container">
             <section className="flex-1 flex flex-col border
             border-purple-300 rounded-md p-10 space-y-2">
-                <Textarea 
-                value={story}
-                onChange={(e) => setStory(e.target.value)}
-                className="flex-1 text-black"
-                 placeholder="Write a story about a robot and a human who become friends"
+                <Textarea
+                    value={story}
+                    onChange={(e) => setStory(e.target.value)}
+                    className="flex-1 text-black"
+                    placeholder="Write a story about a robot and a human who become friends"
                 />
                 <Select
-                onValueChange={value => setPages(parseInt(value))}
+                    onValueChange={value => setPages(parseInt(value))}
                 >
                     <SelectTrigger>
                         <SelectValue placeholder="How many pages should the story be?"
@@ -36,15 +39,29 @@ function StoryWriter() {
                         ))}
                     </SelectContent>
                 </Select>
-                <Button 
-                disabled={!story || !pages}
-                className="w-full" size='lg'>
+                <Button
+                    disabled={!story || !pages}
+                    className="w-full" size='lg'>
                     Generate Story
                 </Button>
 
             </section>
             <section className="flex-1 pb-5 mt-5">
-
+                <div className="flex flex-col-reverse 
+                w-full space-y-2 bg-gray-800 rounded-md
+                 text-gray-200 font-mono p-10 h-96 overflow-y-auto">
+                    <div>
+                        {runFinished == null && (
+                            <>
+                                <p className="animate-pulse mr-5">I'm waiting for you to generate a story above...</p>
+                                <br />
+                            </>
+                        )}
+                        <span className="mr-5">{">>"}</span>
+                        {progress}
+                    </div>
+                    {/* Current tool */}
+                </div>
             </section>
         </div>
     )
